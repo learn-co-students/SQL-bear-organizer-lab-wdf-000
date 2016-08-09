22a5849a082d4e14845ba84423bc7f1f7256befe
Flatiron School <github email address>
2016-08-09 13:38:38 -0400

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 45, "M", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 12, "M", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 12, "F", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 50, "F", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 80, "F", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 90, "M", "black", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 60, "F", "brown", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (NULL, 60, "F", "brown", "happy", "true")
