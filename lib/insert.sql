0bae2dadd19ed43ab8314a9d4a859d63f93b9408
Kelene Chung <simoy93@gmail.com>
2016-08-17 13:30:34 -0400

    SQL bear organizer completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES 
+(1, 'Mr. Chocolate', 4, 'M', 'brown', 'chill', 1),
+(2, 'Rowdy', 10, 'M', 'white', 'happy', 1),
+(3, 'Tabitha', 3, 'F', 'red', 'sleepy', 1),
+(4, 'Sergeant Brown', 20, 'M', 'brown', 'angry', 1),
+(5, 'Melissa', 10, 'F', 'brown', 'chill', 1),
+(6, 'Grinch', 9, 'M', 'black', 'happy', 1),
+(7, 'Wendy', 9, 'F', 'black', 'sad', 1),
+(8,  null, 25, 'M', 'white', 'dead', 0);
