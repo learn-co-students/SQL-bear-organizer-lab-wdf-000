2d1b825d9eab31a11ddc3cff49002e96831d7be0
Naveed Shah <naveed@tayybah.com>
2016-08-19 17:01:11 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,33 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears where GENDER='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT name, age FROM bears where alive=1 ORDER BY age"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE age = (SELECT MAX(age) from bears) LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "select name, age from bears where age = (select MIN(age) from bears);"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "select color, count(color) from bears WHERE color = (select MAX(color) from bears);"
 end
 
+# "select MAX(color), COUNT(color) from bears;"
+
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "select COUNT(name) from bears where temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "select * from bears where temperament= 'aggressive';"
 end
